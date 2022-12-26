a694d5b1dc21afaa5a595f1b674199a1a5845b4b
DoubleSec <awzack@gmail.com>
2022-12-26 12:59:32 -0500

    working input layer

--- wn/net.py
+++ wn/net.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
 
+from .data import DataInterface
+
 
 class Time2Vec(nn.Module):
     def __init__(self, size: int):
@@ -10,10 +12,10 @@ class Time2Vec(nn.Module):
         self.size = size
         # TKTK Is this the right initialization?
         self.weight = nn.Parameter(
-            torch.normal(torch.zeros([1, self.size]), 1, requires_grad=True)
+            torch.normal(torch.zeros([1, self.size]), 1.0)
         )
         self.bias = nn.Parameter(
-            torch.normal(torch.zeros([1, self.size]), 1, requires_grad=True)
+            torch.normal(torch.zeros([1, self.size]), 1.0)
         )
 
     def forward(self, x):
@@ -22,6 +24,55 @@ class Time2Vec(nn.Module):
         periodic_terms = torch.sin(self.weight[:, 1:] * x + self.bias[:, 1:])
 
         return torch.cat([linear_term, periodic_terms], dim=1)
+
+
+class TabularInputLayer(nn.Module):
+    def __init__(
+        self,
+        interface: DataInterface,
+        col_encoding_size: int,
+        embedding_size: int,
+    ):
+
+        super().__init__()
+
+        self.interface = interface
+        self.embedding_size = embedding_size
+        self.col_encoding_size = col_encoding_size
+
+        # Initialize the embedding layer for each column
+        self.embedding = nn.ModuleDict()
+        for k, v in self.interface.type_map.items():
+
+            if v[0] == "numeric":
+                self.embedding[k] = nn.Linear(v[2].shape[0], embedding_size)
+
+            elif v[0] == "categorical":
+                self.embedding[k] = nn.Embedding(len(v[1]), embedding_size)
+
+            else:
+                self.embedding[k] = Time2Vec(embedding_size)
+
+        # Initialize the column encoding
+        self.col_encoding = nn.Parameter(torch.normal(torch.zeros([len(self.interface), self.col_encoding_size]), 1.0))
+        # self.col_encoding = nn.Embedding(len(self.interface), self.col_encoding_size)
+
+    def forward(self, x: dict):
+
+        # This will break if the batch size is 1
+        # n x e x s
+        x = torch.stack([self.embedding[k](v).squeeze() for k, v in x.items()], dim=-1)
+        
+        # permute tensor to n x s x e
+        x = torch.permute(x, [0, 2, 1])
+
+        # s x e
+        exp_col_encoding = self.col_encoding.unsqueeze(0).expand([x.shape[0], -1, -1])
+
+        x = torch.cat([x, exp_col_encoding], dim=-1)
+
+        return x
+
 
 
 if __name__ == "__main__":
